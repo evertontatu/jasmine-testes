41eed0e4030aea8837372ff95e889ff39291d5cb
Everton Boni <evertonferreira.de@gmail.com>
2016-12-08 16:01:35 -0200

    Data Builder

--- spec/ConsultaSpec.js
+++ spec/ConsultaSpec.js
@@ -1,29 +1,40 @@
 describe("Consulta", function(){
 
-    it("nao deve cobrar nada se for um retorno", function(){
-        var everton = new Paciente("Everton", 28, 72, 1.80);
-        var consulta = new Consulta(everton,[], true, true);
-
-        expect(consulta.preco()).toEqual(0);
+    var everton;
+    beforeEach(function(){
+        everton = new PacienteBuilder().constroi();
     });
 
-    it("deve cobrar 25 por cada procedimento comum", function(){
-        var everton = new Paciente("Everton", 28, 72, 1.80);
-        var consulta = new Consulta(everton, ["proc1", "proc2", "proc3"], false, false);
+    describe("consultas do tipo retorno",function(){
+        it("nao deve cobrar nada se for um retorno", function(){
+            var consulta = new Consulta(everton,[], true, true);
 
-        expect(consulta.preco()).toEqual(75);
+            expect(consulta.preco()).toEqual(0);
+        });
     });
 
-    it("deve cobrar preco especifico dependendo do procedimento", function(){
-        var everton = new Paciente("Everton", 28, 72, 1.80);
-        var consulta = new Consulta(everton, ["proc1", "raio-x", "proc2", "gesso"], false, false);
 
-        expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+    describe("consultas com procedimento",function(){
+        it("deve cobrar 25 por cada procedimento comum", function(){
+            var consulta = new Consulta(everton, ["proc1", "proc2", "proc3"], false, false);
+
+            expect(consulta.preco()).toEqual(75);
+        });
+
+        it("deve cobrar preco especifico dependendo do procedimento", function(){
+            var consulta = new Consulta(everton, ["proc1", "raio-x", "proc2", "gesso"], false, false);
+
+            expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+        });
+
+
+        it("deve calcular o imc", function(){
+            expect(everton.imc()).toEqual(72/(1.80*1.80));
+        });
+
+        it("deve calcular os batimentos", function(){
+            expect(everton.batimentos()).toEqual(28 * 365 * 24 * 60 * 80);
+        });
     });
 
-    it("deve calcular o imc", function(){
-        var everton = new Paciente("Everton", 28, 72, 1.80);
-
-        expect(everton.imc()).toEqual(72/(1.80*1.80));
-    });
 });
